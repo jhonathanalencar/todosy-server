beacc92fc17154f0b31e1e90dca3c74695480662
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2022-12-23 19:33:15 -0300

    chore: add commitlint, lint-staged and husky

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT ?? 3333;
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
