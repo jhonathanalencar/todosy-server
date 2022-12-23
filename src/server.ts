2790d926e6bb163a910bcb36e8e44793ee7c0103
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2022-12-23 18:06:40 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
